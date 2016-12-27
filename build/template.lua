e07a3b485480e99d1ddd1aeea58be8aaee39cb2a
Sam <sam@sbseltzer.net>
2016-12-27 01:44:24 -0500

    Added project.lua - modified ignore files

---
+++ build/template.lua
@@ -0,0 +1,8 @@
+return {
+  projectname = "Game", -- Output Project/Solution file name
+  execname = "Game", -- Compiled executable name
+  targetdir = "bin", -- Executable output folder (starting from base project directory)
+  libdir = "lib", -- Compile-time libary directory name (starting from base project directory)
+  language = "C", -- Project language
+  console = true -- Whether to attach a console window (Not important on Linux)
+}
