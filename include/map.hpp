d83ec05e753b755df5ad1ae0c712bb2c6de0cc04
Sam Seltzer-Johnston <sam@sbseltzer.net>
2017-02-24 12:21:54 -0500

    Hotloading complete - MapParser class is officially gone.

--- include/map.hpp
+++ include/map.hpp
@@ -5,6 +5,7 @@
 static const orxU32 MAP_MIN_GRIDSIZE = 1;
 /*! Minimum grid size for processing physics */
 static const orxU32 MAP_MIN_GRIDSIZE_PHYSICS = 8;
+static const orxU32 MAP_MAX_ALIAS_LENGTH = 64;
 
 /*! Map storage */
 static orxBANK *spstMapBank;
@@ -22,12 +23,147 @@ typedef struct MapData {
   orxHASHTABLE *pstTileAliasTable;  /*! alias -> tile section name */
   orxHASHTABLE *pstTileIndexTable; /*! tile alias -> tile index */
   orxHASHTABLE *pstObjectPosTable; /*! position -> object list */
-  orxBODY* pstBody;                /*! Body to add tile BodyParts to */
+  orxOBJECT *pstObject;
+  orxBODY *pstBody;                /*! Body to add tile BodyParts to */
 } MapData;
 
-orxU64 map_HasChanged(MapData* _pstMap) {
-  // My thought was to generate a hash of the map and all its dependencies, but I'm realizing that could explode in calculation time. It also would require re-checking all data related to all maps.
-  // I'd need to check the tile table name, the body name, the map layout, the contents of the tile table, the contents of the body, the grid size, and the object table
+typedef struct MapLayoutParser {
+  const MapData *pstMap;
+  const orxU32 u32Length;
+  orxU32 u32Index;
+  orxVECTOR vCurrentGridPos;
+  orxVECTOR vCurrentWorldPos;
+  orxSTRING zCurrentTileName;
+} MapLayoutParser;
+
+MapLayoutParser map_CreateLayoutParser(const MapData* pstMap) {
+  MapLayoutParser parser = {pstMap, orxString_GetLength(pstMap->zMapLayout), 0};
+  orxVector_Set(&parser.vCurrentGridPos, 0, 0, 0);
+  orxVector_Set(&parser.vCurrentWorldPos, 0, 0, 0);
+  parser.zCurrentTileName = (orxSTRING) orxNULL;
+  return parser;
+}
+
+/*! Return false if reaches end of string - store linebreaks */
+static orxBOOL map_ParseLayoutWhiteSpace(MapLayoutParser *pstParser, orxU32 *pu32LineBreaks) {
+  if (pstParser->u32Index >= pstParser->u32Length) { return orxFALSE; }
+  orxCHAR ch = pstParser->pstMap->zMapLayout[pstParser->u32Index];
+  while ( ( ch == ' ' || ch == '\r' || ch == '\n' ) && pstParser->u32Index < pstParser->u32Length ) {
+    // orxLOG("Skipping whitespace: pstParser->pstMap->zMapLayout[%u] = '%c'", pstParser->u32Index, ch);
+    if (ch == '\n') { (*pu32LineBreaks)++; }
+    ch = pstParser->pstMap->zMapLayout[++pstParser->u32Index];
+  }
+  return ( pstParser->u32Index < pstParser->u32Length );
+}
+
+// Assumes skipWhiteSpace has been called before it.
+// If the index is on whitespace, it simply won't make any progress.
+static orxBOOL map_ParseLayoutTile(MapLayoutParser *pstParser) {
+  if (pstParser->u32Index >= pstParser->u32Length) { return orxFALSE; }
+  orxCHAR ch = pstParser->pstMap->zMapLayout[pstParser->u32Index];
+  orxU32 u32StartIndex = pstParser->u32Index;
+  while (ch != ' ' && ch != '\r' && ch != '\n' && pstParser->u32Index < pstParser->u32Length) {
+    // orxLOG("Reading tileID: mapString[%u] = '%c'", pstParser->u32Index, ch);
+    ch = pstParser->pstMap->zMapLayout[++pstParser->u32Index];
+  }
+  orxU32 u32SubLength = pstParser->u32Index - u32StartIndex;
+  orxASSERT(u32SubLength < MAP_MAX_ALIAS_LENGTH);
+  orxCHAR zTileAlias[MAP_MAX_ALIAS_LENGTH];
+  strncpy(zTileAlias, pstParser->pstMap->zMapLayout + u32StartIndex, u32SubLength);
+  zTileAlias[u32SubLength] = '\0';
+  pstParser->zCurrentTileName = (orxSTRING)orxHashTable_Get(pstParser->pstMap->pstTileAliasTable, (orxU64)orxString_GetID(zTileAlias));
+  // orxLOG("tileID is substring from %u to %u: %s %s", u32StartIndex, pstParser->u32Index, zTileAlias, pstParser->zCurrentTileName);
+  return ( pstParser->u32Index < pstParser->u32Length );
+}
+
+orxBOOL map_ParseLayout(MapLayoutParser *pstParser) {
+  // orxLOG("Parsing next tile");
+  // Skip whitespace, keeping a count of linebreaks along the way.
+  orxU32 u32LineBreaks = 0;
+  orxBOOL bContinue = map_ParseLayoutWhiteSpace(pstParser, &u32LineBreaks);
+
+  // orxLOG("u32LineBreaks: %u", u32LineBreaks);
+  pstParser->vCurrentGridPos.fY += u32LineBreaks;
+
+  // If a linebreak was hit, reset tilespace column to 0.
+  if (u32LineBreaks > 0) { pstParser->vCurrentGridPos.fX = 0; }
+  else { pstParser->vCurrentGridPos.fX++; }
+
+  // Update the world positions
+  pstParser->vCurrentWorldPos.fX = pstParser->vCurrentGridPos.fX * pstParser->pstMap->vGridUnitSize.fX;
+  pstParser->vCurrentWorldPos.fY = pstParser->vCurrentGridPos.fY * pstParser->pstMap->vGridUnitSize.fY;
+
+  // orxLOG("Tile Position: <%f %f %f> <%f %f %f>",
+  //        pstParser->vCurrentGridPos.fX,
+  //        pstParser->vCurrentGridPos.fY,
+  //        pstParser->vCurrentGridPos.fZ,
+  //        pstParser->vCurrentWorldPos.fX,
+  //        pstParser->vCurrentWorldPos.fY,
+  //        pstParser->vCurrentWorldPos.fZ);
+  // If we hit the end of the string, return as such.
+  if (!bContinue) { return orxFALSE; }
+
+  // Update current tile ID and return false if it detects end of string.
+  bContinue = map_ParseLayoutTile(pstParser);
+  if (!bContinue) {
+    orxConfig_PopSection();
+  }
+  return bContinue;
+}
+
+void map_AddPhysicsForTile(const orxSTRING _zTileName, orxBODY *_pstWorldBody, orxVECTOR *vGridUnitSize, orxVECTOR *_vTopLeft) {
+  const orxSTRING zPartType;
+  if (vGridUnitSize->fX < MAP_MIN_GRIDSIZE_PHYSICS || vGridUnitSize->fY < MAP_MIN_GRIDSIZE_PHYSICS) {
+    orxLOG("WARNING: Attempted to add physics for tile with vGridUnitSize of %f,%f (<= %u)! Doing so is unsafe. Skipping...", vGridUnitSize->fX, vGridUnitSize->fY, MAP_MIN_GRIDSIZE_PHYSICS);
+    return;
+  }
+  orxASSERT(_zTileName);
+  orxASSERT(_pstWorldBody);
+  orxConfig_PushSection(_zTileName);
+  zPartType = orxConfig_GetString("Type");
+  if (orxString_Compare(zPartType, "") != 0) {
+    if (orxString_Compare(zPartType, "box") == 0) {
+      orxConfig_SetVector("TopLeft", _vTopLeft);
+      orxVECTOR bottomRight = {0};
+      orxVector_Set(&bottomRight, _vTopLeft->fX + vGridUnitSize->fX, _vTopLeft->fY + vGridUnitSize->fY, 0);
+      orxConfig_SetVector("BottomRight", &bottomRight);
+    } else if (orxString_Compare(zPartType, "sphere") == 0) {
+      orxVECTOR center = {0};
+      orxVector_Set(&center, _vTopLeft->fX + vGridUnitSize->fX / 2.0, _vTopLeft->fY + vGridUnitSize->fY / 2.0, 0);
+      orxConfig_SetVector("BottomRight", &center);
+    } else if (orxString_Compare(zPartType, "mesh") == 0) {
+      orxLOG("Mesh type unsupported: %s", _zTileName);
+    }
+    orxBody_AddPartFromConfig(_pstWorldBody, _zTileName);
+  }
+  orxConfig_PopSection();
+}
+
+typedef struct MapObjectParser {
+  const orxSTRING zMapName;
+  orxU32 u32KeyIndex;
+} MapObjectParser;
+
+MapObjectParser map_CreateObjectParser(const orxSTRING _zMapName) {
+  return {_zMapName, 0};
+}
+
+orxSTATUS map_NextObjectListPosition(MapObjectParser *_pstParser, const orxSTRING *_zKeyOut, orxVECTOR *vPositionOut) {
+  orxSTATUS status = orxSTATUS_FAILURE;
+  orxConfig_PushSection(_pstParser->zMapName);
+  orxVector_Set(vPositionOut, 0, 0, 0);
+  const orxU32 u32KeyCount = orxConfig_GetKeyCounter();
+  while ((_pstParser->u32KeyIndex < u32KeyCount)) {
+    const orxSTRING zKeyName = orxConfig_GetKey(_pstParser->u32KeyIndex);
+    _pstParser->u32KeyIndex++;
+    if (orxString_ToVector(zKeyName, vPositionOut, orxNULL) == orxSTATUS_SUCCESS) {
+      *_zKeyOut = zKeyName;
+      status = orxSTATUS_SUCCESS;
+      break;
+    }
+  }
+  orxConfig_PopSection();
+  return status;
 }
 
 /*! Load the specified section into a hashtable */
@@ -88,14 +224,48 @@ MapData *map_CreateMapData(const orxSTRING _zMapName) {
 
 void map_DeleteMapData(MapData *_pstMap) {
   orxASSERT(_pstMap);
+  orxObject_SetLifeTime(_pstMap->pstObject, orxFLOAT_0);
   orxLinkList_Remove((orxLINKLIST_NODE *)_pstMap);
   orxBank_Free(spstMapBank, _pstMap);
+}
+
+void map_Generate(MapData *pstMapData) {
+  MapLayoutParser stParser = map_CreateLayoutParser(pstMapData);
+  pstMapData->pstObject = orxObject_CreateFromConfig(pstMapData->zMapName);
+  pstMapData->pstBody = orxOBJECT_GET_STRUCTURE(pstMapData->pstObject, BODY);
+  while (map_ParseLayout(&stParser)) {
+    if (stParser.zCurrentTileName == orxNULL) continue;
+    map_AddPhysicsForTile(stParser.zCurrentTileName, pstMapData->pstBody, &pstMapData->vGridUnitSize, &stParser.vCurrentWorldPos);
+    orxOBJECT *object = orxObject_CreateFromConfig(stParser.zCurrentTileName);
+    orxObject_SetPosition(object, &stParser.vCurrentWorldPos);
+    orxObject_SetOwner(object, pstMapData->pstObject);
+  }
+  MapObjectParser objectParser = map_CreateObjectParser(pstMapData->zMapName);
+  orxVECTOR objectPos = {0};
+  const orxSTRING objectListKey;
+  while (map_NextObjectListPosition(&objectParser, &objectListKey, &objectPos) != orxSTATUS_FAILURE) {
+    objectPos.fX *= pstMapData->vGridUnitSize.fX;
+    objectPos.fY *= pstMapData->vGridUnitSize.fY;
+    objectPos.fZ *= pstMapData->vGridUnitSize.fZ;
+    orxConfig_PushSection(objectParser.zMapName);
+    orxS32 numObjects = orxConfig_GetListCounter(objectListKey);
+    for (orxS32 i = 0; i < numObjects; i++) {
+      const orxSTRING objectListValue = orxConfig_GetListString(objectListKey, i);
+      if (orxConfig_HasSection(objectListValue)) {
+        orxOBJECT *object = orxObject_CreateFromConfig(objectListValue);
+        orxObject_SetPosition(object, &objectPos);
+        orxObject_SetOwner(object, pstMapData->pstObject);
+      }
+    }
+    orxConfig_PopSection();
+  }
 }
 
 void map_Reload(MapData *_pstMap) {
   const orxSTRING zMapName = _pstMap->zMapName;
   map_DeleteMapData(_pstMap);
   *_pstMap = *(map_CreateMapData(zMapName));
+  map_Generate(_pstMap);
   // return _pstMap;
 }
 
@@ -133,198 +303,11 @@ orxSTATUS map_Exit() {
 }
 
 
-struct MapParser {
-
-public:
-  orxVECTOR gridPosition;
-  orxVECTOR tileTopLeft;
-  orxSTRING tileSection;
-
-private:
-  const orxSTRING mapName;
-  orxU32 index;
-  orxU32 length;
-  orxU32 gridSize;
-  std::string mapString;
-  orxHASHTABLE *tileTable;
-
-  void readConfig(const orxSTRING sectionName) {
-    const orxSTRING tileTableSection;
-
-    // Read map info from config
-    orxConfig_PushSection(sectionName);
-    gridSize = orxConfig_GetU32("GridSize");
-    mapString = orxConfig_GetString("Map");
-    tileTableSection = orxConfig_GetString("Tiles");
-    orxConfig_PopSection();
-
-    // Populate the tile table using the section name specified by the map
-    tileTable = map_LoadTileTable(tileTableSection);
-    orxASSERT(tileTable);
-  }
-
-  orxBOOL skipWhiteSpace(orxU32 &lineBreaks) {
-    if (index >= length) { return orxFALSE; }
-    orxCHAR ch = mapString[index];
-    while ( ( ch == ' ' || ch == '\r' || ch == '\n' ) && index < length ) {
-      // orxLOG("Skipping whitespace: mapString[%u] = '%c'", index, ch);
-      if (ch == '\n') { lineBreaks++; }
-      ch = mapString[++index];
-    }
-    return ( index < length );
-  }
-
-  // Assumes skipWhiteSpace has been called before it.
-  // If the index is on whitespace, it simply won't make any progress.
-  orxBOOL updateTileID() {
-    if (index >= length) { return orxFALSE; }
-    orxCHAR ch = mapString[index];
-    orxU32 tileStartIndex = index;
-    while (ch != ' ' && ch != '\r' && ch != '\n' && index < length) {
-      // orxLOG("Reading tileID: mapString[%u] = '%c'", index, ch);
-      ch = mapString[++index];
-    }
-    std::string tileID = mapString.substr(tileStartIndex, index - tileStartIndex);
-    tileSection = (orxSTRING)orxHashTable_Get(tileTable, (orxU64)orxString_GetID(tileID.c_str()));
-    // orxLOG("tileID is substring from %u to %u: %s %s", tileStartIndex, index, tileID.c_str(), tileSection);
-    return ( index < length );
-  }
-
-  orxBOOL Setup() {
-    index = 0;
-    orxVector_Set(&gridPosition, 0.0, 0.0, 0.0);
-    orxVector_Set(&tileTopLeft, 0.0, 0.0, 0.0);
-    tileSection = (orxSTRING) orxNULL;
-
-    readConfig(mapName);
-    length = orxString_GetLength(mapString.c_str());
-
-    // orxLOG("mapString: %u\n\"%s\"", length, mapString.c_str());
-    orxConfig_PushSection(mapName);
-    return true;
-  }
-
-public:
-  MapParser(const orxSTRING mapSection) :
-    mapName(mapSection),
-    tileTable((orxHASHTABLE*)orxNULL),
-    gridPosition(orxVECTOR_0),
-    tileTopLeft(orxVECTOR_0),
-    tileSection((orxSTRING)orxNULL),
-    index(0), length(0), gridSize(0), mapString("") {
-    Setup();
-  }
-
-  orxU32 getGridSize() {
-    return gridSize;
-  }
-  orxBOOL nextTile() {
-    // orxLOG("Parsing next tile");
-    // Skip whitespace, keeping a count of linebreaks along the way.
-    orxU32 lineBreaks = 0;
-    orxBOOL continueParsing = skipWhiteSpace(lineBreaks);
-    // orxLOG("lineBreaks: %u", lineBreaks);
-    gridPosition.fY += lineBreaks;
-    // If a linebreak was hit, reset tilespace column to 0.
-    if (lineBreaks > 0) { gridPosition.fX = 0; }
-    else { gridPosition.fX++; }
-    // If we hit the end of the string, return as such.
-    if (!continueParsing) { return orxFALSE; }
-    // Update the world positions
-    tileTopLeft.fX = gridPosition.fX * gridSize;
-    tileTopLeft.fY = gridPosition.fY * gridSize;
-
-    // Update current tile ID and return false if it detects end of string.
-    continueParsing = updateTileID();
-    if (!continueParsing) {
-      orxConfig_PopSection();
-    }
-    return continueParsing;
-  }
-};
-
-void map_AddPhysicsForTile(const orxSTRING _zTileName, orxBODY *_pstWorldBody, orxU32 u32GridSize, orxVECTOR *_vTopLeft) {
-  const orxSTRING zPartType;
-  if (u32GridSize < MAP_MIN_GRIDSIZE_PHYSICS) {
-    orxLOG("WARNING: Attempted to add physics for tile with u32GridSize of %u (<= %u)! Doing so is unsafe. Skipping...", u32GridSize, MAP_MIN_GRIDSIZE_PHYSICS);
-    return;
-  }
-  orxASSERT(_zTileName);
-  orxASSERT(_pstWorldBody);
-  orxConfig_PushSection(_zTileName);
-  zPartType = orxConfig_GetString("Type");
-  if (orxString_Compare(zPartType, "") != 0) {
-    if (orxString_Compare(zPartType, "box") == 0) {
-      orxConfig_SetVector("TopLeft", _vTopLeft);
-      orxVECTOR bottomRight = {0};
-      orxVector_Set(&bottomRight, _vTopLeft->fX + u32GridSize, _vTopLeft->fY + u32GridSize, 0);
-      orxConfig_SetVector("BottomRight", &bottomRight);
-    } else if (orxString_Compare(zPartType, "sphere") == 0) {
-      orxVECTOR center = {0};
-      orxVector_Set(&center, _vTopLeft->fX + u32GridSize / 2.0, _vTopLeft->fY + u32GridSize / 2.0, 0);
-      orxConfig_SetVector("BottomRight", &center);
-    } else if (orxString_Compare(zPartType, "mesh") == 0) {
-      orxLOG("Mesh type unsupported: %s", _zTileName);
-    }
-    orxBody_AddPartFromConfig(_pstWorldBody, _zTileName);
-  }
-  orxConfig_PopSection();
-}
-
-typedef struct MapObjectParser {
-  const orxSTRING zMapName;
-  orxU32 u32KeyIndex;
-} MapObjectParser;
-
-MapObjectParser map_CreateObjectParser(const orxSTRING _zMapName) {
-  return {_zMapName, 0};
-}
-
-orxSTATUS map_NextObjectListPosition(MapObjectParser *_pstParser, const orxSTRING *_zKeyOut, orxVECTOR *vPositionOut) {
-  orxSTATUS status = orxSTATUS_FAILURE;
-  orxConfig_PushSection(_pstParser->zMapName);
-  orxVector_Set(vPositionOut, 0, 0, 0);
-  const orxU32 u32KeyCount = orxConfig_GetKeyCounter();
-  while ((_pstParser->u32KeyIndex < u32KeyCount)) {
-    const orxSTRING zKeyName = orxConfig_GetKey(_pstParser->u32KeyIndex);
-    _pstParser->u32KeyIndex++;
-    if (orxString_ToVector(zKeyName, vPositionOut, orxNULL) == orxSTATUS_SUCCESS) {
-      *_zKeyOut = zKeyName;
-      status = orxSTATUS_SUCCESS;
-      break;
-    }
-  }
-  orxConfig_PopSection();
-  return status;
-}
 
 // Implementation of map generation
 void map_LoadMapData(const orxSTRING mapName) {
   if (mapName != orxNULL) {
-    MapParser parser(mapName);
-    const orxOBJECT *map = orxObject_CreateFromConfig(mapName);
-    orxBODY *body = orxOBJECT_GET_STRUCTURE(map, BODY);
-    while (parser.nextTile()) {
-      if (parser.tileSection == orxNULL) continue;
-      map_AddPhysicsForTile(parser.tileSection, body, parser.getGridSize(), &parser.tileTopLeft);
-      orxOBJECT *object = orxObject_CreateFromConfig(parser.tileSection);
-      orxObject_SetPosition(object, &parser.tileTopLeft);
-    }
-    MapObjectParser objectParser = map_CreateObjectParser(mapName);
-    orxVECTOR objectPos = {0};
-    const orxSTRING objectListKey;
-    while (map_NextObjectListPosition(&objectParser, &objectListKey, &objectPos) != orxSTATUS_FAILURE) {
-      orxVector_Mulf(&objectPos, &objectPos, parser.getGridSize());
-      orxConfig_PushSection(objectParser.zMapName);
-      orxS32 numObjects = orxConfig_GetListCounter(objectListKey);
-      for (orxS32 i = 0; i < numObjects; i++) {
-        const orxSTRING objectListValue = orxConfig_GetListString(objectListKey, i);
-        if (orxConfig_HasSection(objectListValue)) {
-          orxOBJECT *object = orxObject_CreateFromConfig(objectListValue);
-          orxObject_SetPosition(object, &objectPos);
-        }
-      }
-      orxConfig_PopSection();
-    }
+    MapData* pstMapData = map_CreateMapData(mapName);
+    map_Generate(pstMapData);
   }
 }
